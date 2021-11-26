38fe34cea26e4e1a2683f965af8c4cc62fc73b0a
Ubuntu <robertwhitney97@gmail.com>
2021-11-26 21:45:26 +0000

    stuff

---
+++ bobcat_msgs/include/serial_msg_common.h
@@ -0,0 +1,52 @@
+
+#define SERIAL_MSG_START_INDICATOR '<'
+#define SERIAL_MSG_END_INDICATOR '>'
+#define SERIAL_MSG_DELIMITER ','
+#define SERIAL_MSG_MAX_LEN 255
+
+enum class DAQ_SERIAL_MSG {
+    NOT_A_MSG = 0,
+    PING,
+    ENGAGE_FAILSAFE,
+    JOYSTICK_MSG,
+    GPS_MSG,
+    MOTOR_CMD,
+    LED_CMD
+};
+
+enum class DAQ_SERIAL_MSG_LEN {
+    NOT_A_MSG = 1,
+    PING = 1,
+    ENGAGE_FAILSAFE = 1,
+    JOYSTICK_MSG = 5,
+    GPS_MSG = 1,
+    MOTOR_CMD = 5,
+    LED_CMD = 1
+};
+
+enum class VEHICLE_STATE {
+    MANUAL = 0,
+    ACRO,
+    AUTO
+};
+
+enum class BOBCAT_TELEM_MSG {
+    NOT_A_MSG = 0,
+    PING,
+    ENGAGE_FAILSAFE,
+    RETURN_2_HOME,
+    LOITER,
+    SBUS_LOG,
+    GPS_LOG
+};
+
+enum class BOBCAT_TELEM_MSG_LEN {
+    NOT_A_MSG = 1,
+    PING = 1,
+    ENGAGE_FAILSAFE = 1,
+    RETURN_2_HOME = 1,
+    LOITER = 1,
+    SBUS_LOG = 1,
+    GPS_LOG = 1
+};
+
